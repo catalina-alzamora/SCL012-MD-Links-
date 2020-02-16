ad5b7a1133535dedb7534ec645693c793b4d4884
catalina-alzamora <alzamoracatalina@gmail.com>
2020-02-16 10:10:11 -0400

    función leer links

--- md-links.js
+++ md-links.js
@@ -5,14 +5,30 @@ const path = require('path');
 const absolutePath = path.normalize(path.resolve(file)); // normalize() arregla la ruta. resolve() la hace absoluta
 
 
-mdLinks.readingFile(absolutePath).then(absolutePath => console.log(absolutePath)); // llamando función para leer archivo
+// mdLinks.readingFile(absolutePath).then(absolutePath => console.log(absolutePath)); // llamando función para leer archivo
 
-/*function verifyMdFile(absolutePath) { // Función para detectar archivos tipo .md
+function verifyMdFile(absolutePath) { // Función para detectar archivos tipo .md
   if (path.extname(absolutePath) === '.md') {
     console.log('es md')
-    mdLinks.readingFile(absolutePath).then(absolutePath => console.log(absolutePath)); 
+    mdLinks.readingFile(absolutePath).then(data => {
+      let links = [];
+      const renderer = new marked.Renderer();
+      renderer.link = function (href, title, text) {
+        links.push({
+          href: href,
+          text: text,
+          file: file
+        });
+      };
+      marked(data, {
+        renderer: renderer
+      });
+      console.log(links);
+    }).catch((err) => {
+      console.log(err);
+    });
   } else {
     console.log('error. Ingrese un archivo .md');
   }
 };
-verifyMdFile(absolutePath);*/
+verifyMdFile(absolutePath);
